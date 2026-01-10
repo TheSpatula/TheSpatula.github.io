1dab093596ac0f03700c0b211769e9429b0513b4
TheSpatula <thespatulariansthespatula@gmail.com>
2026-01-10 17:48:32 +0100

    C: update UI

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 function calculate() {
-    const pizzas = document.getElementById("pizzas").value;
+  const pizzas = Number(document.getElementById("pizzas").value);
 
-    const flour = pizzas * 100;
-    const water = pizzas * 60;
-    const salt = pizzas * 2;
-    const yeast = pizzas * 1;
+  const ingredients = [
+    { name: "Flour", value: pizzas * 100, unit: "g" },
+    { name: "Water", value: pizzas * 60, unit: "g" },
+    { name: "Salt", value: pizzas * 2, unit: "g" },
+    { name: "Yeast", value: pizzas * 1, unit: "g" }
+  ];
 
-    document.getElementById("result").innerHTML =  `
-    <h2>Ingredients</h2>
-    <p>Flour: ${flour} g</p>
-    <p>Water: ${water} g</p>
-    <p>Salt: ${salt} g</p>
-    <p>Yeast: ${yeast} g</p>
-  `;
+  const list = document.getElementById("ingredients");
+  list.innerHTML = "";
+
+  ingredients.forEach(item => {
+    const li = document.createElement("li");
+    li.innerHTML = `
+      <span>${item.name}</span>
+      <span>${item.value} ${item.unit}</span>
+    `;
+    list.appendChild(li);
+  });
+
+  document.getElementById("result").classList.remove("hidden");
 }
